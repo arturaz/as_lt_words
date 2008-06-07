68b63944b0a4c450a43b3691dce0fab0a2c525c6
x11 <x11@www.lan>
2008-06-07 19:07:20 +0300

    Testing.

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 require File.join(File.dirname(__FILE__), 'lib', 'arturaz', 'as_lt_words.rb')
 
+# Extend classes
 Date.send(:include, Arturaz::AsLtWords::DateExtensions::InstanceMethods)
 Time.send(:include, Arturaz::AsLtWords::TimeExtensions::InstanceMethods)
 Time.send(:private, :ago_as_lt_words, :since_as_lt_words)
